c7243e2f677f4e67484708e06d18c0f007780137
Tadon <Mark.Klocek@GMail.com>
2025-04-27 12:46:12 -0700

    100 devs class 12  boring/class/weekend selector homework

--- 100 devs/Class 12/class12-materials/class-weekend-boring/js/main.js
+++ 100 devs/Class 12/class12-materials/class-weekend-boring/js/main.js
@@ -2,9 +2,16 @@ document.querySelector('#check').addEventListener('click', check)
 
 function check() {
 
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase()
 
   //Conditionals go here
-
-
+  if (day === "tuesday" || day === "thursday"){
+    document.getElementById('placeToSee').innerText = "Class Day"
+  }
+  else if (day === "saturday" || day === "sunday"){
+    document.getElementById('placeToSee').innerText = "Fun Day"
+  }
+  else{
+    document.getElementById('placeToSee').innerText = "Boring Day"
+  }
 }
