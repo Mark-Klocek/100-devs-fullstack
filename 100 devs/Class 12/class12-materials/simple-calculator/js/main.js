5a80d6dc57d754509317ac98ce7c4fcb976e3ca3
Tadon <Mark.Klocek@GMail.com>
2025-04-27 13:54:10 -0700

    100 devs class 12 simple calculator extra work completed

--- 100 devs/Class 12/class12-materials/simple-calculator/js/main.js
+++ 100 devs/Class 12/class12-materials/simple-calculator/js/main.js
@@ -1,31 +1,67 @@
-let total = 0
+// let total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
-document.querySelector('#homeworkAddition').addEventListener('click',add11)
+// document.querySelector('#pumpkin').addEventListener('click', makeZero)
+// document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+// document.querySelector('#zebra').addEventListener('click', add9)
+// document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+// document.querySelector('#homeworkAddition').addEventListener('click',add11)
 
-function makeZero() {
-  total = 0
-  document.querySelector('#placeToPutResult').innerText = total
+// function makeZero() {
+//   total = 0
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function jumanji() {
+//   total = total + 3
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function add9() {
+//   total = total + 9
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+// function sub2() {
+//   total = total - 2
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+// function add11(){
+//   total += 11
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+let result = 0
+
+document.querySelector('#pumpkin').addEventListener('click',zero)
+document.querySelector('#dominosPizza').addEventListener('click',addThree)
+document.querySelector('#zebra').addEventListener('click',addNine)
+document.querySelector('#cantThinkOfAnything').addEventListener('click', minusTwo)
+document.querySelector('#homeworkAddition').addEventListener('click',addEleven)
+
+
+function zero(){
+  result = 0
+  document.querySelector('#placeToPutResult').innerHTML = result
+  document.getElementById('placeToPutResult').style.color = 'red'
+
 }
-
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
+function addThree(){
+  result += 3
+  document.querySelector('#placeToPutResult').innerHTML = result
+  document.getElementById('placeToPutResult').style.color = 'green'
 }
-
-function add9() {
-  total = total + 9
-  document.querySelector('#placeToPutResult').innerHTML = total
+function addNine(){
+  result += 9
+  document.querySelector('#placeToPutResult').innerHTML = result
+  document.getElementById('placeToPutResult').style.color = 'green'
 }
-
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+function minusTwo(){
+  result -= 2
+  document.querySelector('#placeToPutResult').innerHTML = result
+  document.getElementById('placeToPutResult').style.color = 'red'
+  
 }
-function add11(){
-  total += 11
-  document.querySelector('#placeToPutResult').innerHTML = total
+function addEleven(){
+  result += 11
+  document.querySelector('#placeToPutResult').innerHTML = result
+  document.getElementById('placeToPutResult').style.color = 'green'
 }
