c098cbd3c32ef9e10c3180e6966d6cfd1ff14347
Tadon <Mark.Klocek@GMail.com>
2025-04-27 13:32:24 -0700

    100 devs class 12 boring/class/weekend extra work completed

--- 100 devs/Class 12/class12-materials/class-weekend-boring/js/main.js
+++ 100 devs/Class 12/class12-materials/class-weekend-boring/js/main.js
@@ -1,17 +1,36 @@
-document.querySelector('#check').addEventListener('click', check)
+// document.querySelector('#check').addEventListener('click', check)
 
-function check() {
+// function check() {
 
-  const day = document.querySelector('#day').value.toLowerCase()
+//   const day = document.querySelector('#day').value.toLowerCase()
 
-  //Conditionals go here
+//   //Conditionals go here
+//   if (day === "tuesday" || day === "thursday"){
+//     document.getElementById('placeToSee').innerText = "Class Day"
+//   }
+//   else if (day === "saturday" || day === "sunday"){
+//     document.getElementById('placeToSee').innerText = "Fun Day"
+//   }
+//   else{
+//     document.getElementById('placeToSee').innerText = "Boring Day"
+//   }
+// }
+
+document.querySelector('#check').addEventListener('click', dayChecker)
+
+
+function dayChecker(){
+  const day = document.getElementById('day').value.toLowerCase()
   if (day === "tuesday" || day === "thursday"){
     document.getElementById('placeToSee').innerText = "Class Day"
+    document.getElementById('placeToSee').style.color = "green"
   }
   else if (day === "saturday" || day === "sunday"){
-    document.getElementById('placeToSee').innerText = "Fun Day"
+    document.getElementById('placeToSee').innerText = "Weekend"
+    document.getElementById('placeToSee').style.color = "purple"
   }
   else{
     document.getElementById('placeToSee').innerText = "Boring Day"
+    document.getElementById('placeToSee').style.color = "red"
   }
 }
