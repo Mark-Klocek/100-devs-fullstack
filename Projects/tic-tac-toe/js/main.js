4025faf56cf50f54a933a4b528ae8c17824d5c0a
Tadon <Mark.Klocek@GMail.com>
2025-05-05 20:17:56 -0700

    tinkering tic tac toe beginning Game is now functional with restart button working

--- Projects/tic-tac-toe/js/main.js
+++ Projects/tic-tac-toe/js/main.js
@@ -1,21 +1,31 @@
 document.getElementById('gameBoard').addEventListener('click', playerClicksSquare)
+document.getElementById('restart').addEventListener('click',clearTheBoard)
 
 let currentSelection = 2
 function playerClicksSquare(e){
     console.log(e.target.id)
-    if (e.target.style.backgroundImage !== ""){
-        ""
+    if (e.target.classList.contains('square')){
+        if (e.target.style.backgroundImage !== ""){
+            ""
+        }
+        else if (currentSelection == 1){
+            e.target.style.backgroundImage = "url(o.png)"
+            currentSelection = 2
+        }
+        else if (currentSelection == 2){
+            e.target.style.backgroundImage = "url(x.png)"
+            currentSelection = 1
+        }
+        
+        
+        
     }
-    else if (currentSelection == 1){
-        e.target.style.backgroundImage = "url(o.png)"
-        currentSelection = 2
-    }
-    else if (currentSelection == 2){
-        e.target.style.backgroundImage = "url(x.png)"
-        currentSelection = 1
-    }
-    
-    
     
 }
 
+function clearTheBoard(){
+    let squares = document.querySelectorAll('.square')
+    for(i = 0; i < squares.length;i++){
+        squares[i].style.backgroundImage = ''
+    }
+}
