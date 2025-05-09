168992bcfdd7c4c88a7d81297f53260b3d273ce5
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-09 14:52:44 -0700

    100 devs class 20 class work 2

--- 100 devs/Class 20/class20-materials/review-in-paradise-after-paradise/js/main.js
+++ 100 devs/Class 20/class20-materials/review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,16 @@
 // Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+function firstOrLastNumber(array){
+    firstNum = array[0]
+    lastNum = array[array.length - 1]
+    if (firstNum > lastNum){
+        alert('bye')
+    }
+    if (lastNum > firstNum){
+        alert('hi')
+    }
+    if (lastNum == firstNum){
+        alert('We close in an hour')
+    }
+}
+let arr=[4,1,2,3,1,2,3,5,2,1,2,3,10,123,123,112,213]
+firstOrLastNumber(arr)
