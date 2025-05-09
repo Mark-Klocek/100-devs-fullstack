263a0c76fd6fa06b85ad917cb6c5f798b9d1adac
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-09 16:23:13 -0700

    100 devs class 20 class work 4

--- 100 devs/Class 20/class20-materials/review-winter-games/js/main.js
+++ 100 devs/Class 20/class20-materials/review-winter-games/js/main.js
@@ -1,1 +1,13 @@
 //Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+function evenArray(arr){
+    let newArray = []
+    for (i=0;i<arr.length;i++){
+        if (arr[i] % 2 == 0){
+            newArray.push(arr[i])
+        }
+    }
+    return newArray
+}
+
+nums = [23,12,32,54,23,23,123,32,12]
+console.log(evenArray(nums))
