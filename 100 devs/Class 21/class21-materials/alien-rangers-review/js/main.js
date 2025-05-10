0ce72e1519ac6552e7c035b62795facefeeeea20
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-10 13:31:29 -0700

    100 devs class 21 class work 2

--- 100 devs/Class 21/class21-materials/alien-rangers-review/js/main.js
+++ 100 devs/Class 21/class21-materials/alien-rangers-review/js/main.js
@@ -1,9 +1,28 @@
 //Arrays
 
 //Create and array of tv shows. Loop through and print each show to the console
-
+rMovies = ['movie1','movie2','movie3','movie4']
+for (i=0;i<rMovies.length;i++){
+    console.log(rMovies[i])
+}
 //Create and array of numbers
+nArray = [5,23,1,23,54,234,23]
 //Return a new array of numbers that includes every even number from the previous Arrays
+function eArray(arr){
+    let newArray = []
+    for (i = 0; i< arr.length;i++){
+        if (arr[i]%2 ==0){
+            newArray.push(arr[i])
 
+        }
+    }
+    return newArray
+}
+console.log(eArray(nArray))
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function newNum(arr){
+    
+    return (arr[1] + arr.slice(-2)[0])
+}
+console.log(newNum(nArray))
