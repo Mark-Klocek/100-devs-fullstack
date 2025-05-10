095c0eb596cc0fbf2fc70b1e055d4589a568d4f8
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-10 15:17:10 -0700

    100 devs class 21 class work 3

--- 100 devs/Class 21/class21-materials/alien-rangers-review/js/main.js
+++ 100 devs/Class 21/class21-materials/alien-rangers-review/js/main.js
@@ -2,27 +2,34 @@
 
 //Create and array of tv shows. Loop through and print each show to the console
 rMovies = ['movie1','movie2','movie3','movie4']
-for (i=0;i<rMovies.length;i++){
-    console.log(rMovies[i])
-}
+rMovies.forEach(element => console.log(element));
 //Create and array of numbers
 nArray = [5,23,1,23,54,234,23]
 //Return a new array of numbers that includes every even number from the previous Arrays
-function eArray(arr){
-    let newArray = []
-    for (i = 0; i< arr.length;i++){
-        if (arr[i]%2 ==0){
-            newArray.push(arr[i])
 
-        }
-    }
-    return newArray
-}
-console.log(eArray(nArray))
+let newArray = arr => arr.filter(n => n % 2===0)
+console.log(newArray(nArray))
+// function eArray(arr){
+//     let newArray = []
+//     for (i = 0; i< arr.length;i++){
+//         if (arr[i]%2 ==0){
+//             newArray.push(arr[i])
+
+//         }
+//     }
+//     return newArray
+// }
+// console.log(eArray(nArray))
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
-function newNum(arr){
+// function newNum(arr){
     
-    return (arr[1] + arr.slice(-2)[0])
+//     return (arr[1] + arr.slice(-2)[0])
+// }
+// console.log(newNum(nArray))
+let sArray = nArray.sort((a,b) => a-b)
+console.log(sArray)
+function leastAndGreatest(arr){
+    console.log(arr[1]+arr[arr.length - 2])
 }
-console.log(newNum(nArray))
+leastAndGreatest(sArray)
--- 100 devs/Class 21/class21-materials/objects-zeo/js/main.js
+++ 100 devs/Class 21/class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,22 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {
+    'color' : 'black',
+    'buttonCount': 3,
+    'wristWrapLength' : 8,
+    'watchShape': 'square',
+    tellColor(){
+        console.log(stopwatch.color)
+
+    },
+    tellShape(){
+        console.log(stopwatch.watchShape)
+
+    },
+    tellButtonCount(){
+        console.log(stopwatch.buttonCount)
+
+    }
+}
+stopwatch.tellColor()
+stopwatch.tellShape()
+stopwatch.tellButtonCount()
