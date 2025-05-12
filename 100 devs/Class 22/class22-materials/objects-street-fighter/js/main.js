1dbd6e46b8d751ff0a5a5e992cb582d373e3bdcf
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-11 17:05:19 -0700

    100 devs class 22 class work 4

--- 100 devs/Class 22/class22-materials/objects-street-fighter/js/main.js
+++ 100 devs/Class 22/class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,10 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function streeFighter(name,skill,life,damage){
+    this.Name = name
+    this.skill = skill
+    this.totalHp = life
+    this.hitDamage = damage
+}
+
+let Ken = new streeFighter('Ken', 'kick',200,30)
+console.log(Ken)
