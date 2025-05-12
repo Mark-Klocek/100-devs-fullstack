ffa14fe0cc1aeb60b41254423f82e8e418c4cce9
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-12 14:49:37 -0700

    Class 23 class work 4

--- 100 devs/Class 23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ 100 devs/Class 23/class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,21 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+function TonyHawkProSkater(shoes,board,trucks,wheels){
+    this.shoes = shoes;
+    this.board = board;
+    this.trucks = trucks;
+    this.wheels = wheels;
+    this.doTrick = function(){
+        console.log('You did a kick flip!')
+    };
+    this.falconPunch = function(){
+        console.log('You just falcon punched that guy!')
+    };
+    this.pushPushKick = function(){
+        console.log('Push...push..kick... man you"re cruisin')
+    }
+}
+let tony = new TonyHawkProSkater('vans','normal','tall','small')
+tony.falconPunch()
+tony.doTrick()
+tony.pushPushKick()
+console.log(tony)
