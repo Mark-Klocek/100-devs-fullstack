38c45a2816e09cc22a575f1e644127999700835b
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-12 17:34:29 -0700

    Codewars 'simple calculator'

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -116,3 +116,24 @@ function distinct(a) {
 function getASCII(c){
   return c.charCodeAt(0)
 }
+//simple calculator 
+function calculator(a,b,sign){
+  if (sign !== '+' && sign !== '-' && sign !== '*' && sign !=='/'){
+    return "unknown value"
+  }
+  if (isNaN(a) || isNaN(b)){
+    return 'unknown value'
+  }
+  if (sign === '*'){
+    return (a * b)
+  }
+  else if (sign === '+'){
+    return (a + b)
+  }
+  else if (sign === '-'){
+    return (a-b)
+  }
+  else if (sign === '/'){
+    return (a/b)
+  }
+}
