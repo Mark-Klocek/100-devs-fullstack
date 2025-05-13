2223f8d219e80b1b71ebbcd68670ea09d55971ed
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-13 14:31:35 -0700

    kyu 6 :bit counting

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -169,3 +169,14 @@ function spinWords(string){
   }
   return stringArray.join(' ')
 }
+//bit counting
+var countBits = function(n) {
+  let binConvert = n.toString(2)
+  let counter = 0
+  for (i = 0; i < binConvert.length;i++){
+    if (binConvert[i] == 1){
+      counter++
+    }
+  }
+  return counter
+};
