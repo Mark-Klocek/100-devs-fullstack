9c95e9237f4aa5daf3798ff522f9ba1540e51cab
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-14 10:32:57 -0700

    Code wars: you only need one - beginner

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -221,3 +221,11 @@ function isVow(a){
   return newArray
 }
 console.log(isVow(testArray))
+// you only need one - beginner
+testArray = ['what', 'a', 'great', 'kata']
+ 
+let testString = 'kat'
+function check(a, x) {
+  return a.includes(x)
+}
+console.log(check(testArray,testString))
