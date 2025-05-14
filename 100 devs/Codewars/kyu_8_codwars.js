c1fd669b8da571d50fb9b724edaee0c1920aed4d
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-14 11:11:39 -0700

    code wars: lost without a map

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -235,3 +235,9 @@ function addLength(str) {
   return str.split(' ').map((element) => element + ' '+ element.length)
 }
 console.log(addLength(testString))
+//Lost without a map
+testArray = [118,117,120,121,117,98,122,97,120,106,104,116,113,114,113,120,106]
+function maps(x){
+  return x.map(element => element * 2)
+}
+console.log(maps(testArray))
