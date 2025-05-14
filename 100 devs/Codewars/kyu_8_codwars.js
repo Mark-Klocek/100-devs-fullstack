a6e80065fefac4e5b49a0ce78c66db77ca230c12
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-13 19:06:59 -0700

    Code wars: Your order, please

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -180,3 +180,20 @@ var countBits = function(n) {
   }
   return counter
 };
+testString = "is2 Thi1s T4est 3a"
+function order(words){
+  let brokenArray = words.split(' ')
+  let newArray = []
+  brokenArray.map((element) =>{
+    for (i = 0; i < element.length ; i++){
+      if (!isNaN(element[i])){
+          newArray[element[i]-1] = element
+
+      }
+    }
+    
+  })
+  return newArray.join(' ')
+  
+}
+console.log(order(testString))
