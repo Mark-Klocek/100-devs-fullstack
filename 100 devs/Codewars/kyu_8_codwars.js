691784e6fd999255dbe07aedcbdcabaf4994df84
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-15 16:36:06 -0700

    code wars: UEFA EURO 2016

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -349,3 +349,11 @@ function first(arr, n=1) {
   return returnArray
 }
 console.log(first(testArray),4)
+//UEFA EURO 2016
+function uefaEuro2016(teams, scores){
+  if (scores[0] == scores[1]){
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.`
+  }
+  return scores[0] > scores[1] ? `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!` : `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!`
+}
+console.log(uefaEuro2016(['Germany', 'Ukraine'],[2, 3]))
