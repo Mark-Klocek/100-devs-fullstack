798c810ba772a974765105f7c43b21e8c3857c34
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-16 08:40:58 -0700

    code wars: classic hello world (oop)

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -381,3 +381,7 @@ function toCsvText(array) {
    return returnString
 }
 console.log(toCsvText([[ 0, 1, 2, 3, 45 ],[ 10,11,12,13,14 ],[ 20,21,22,23,24 ],  [ 30,31,32,33,34 ]]))
+//Classic hello world
+class Solution{
+}
+Solution.main = function(){console.log('Hello World!')}
