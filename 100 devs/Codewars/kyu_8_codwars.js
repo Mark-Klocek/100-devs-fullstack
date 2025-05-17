60b285ee402cc7874a2d6a6b0b047e5475c5c9f4
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-17 08:53:49 -0700

    code wars: Finish Guess the Number Game

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -438,4 +438,25 @@ class Person {
   }
   
 }
-console.log(Person.greetExtraTerrestrials())
+console.log(Person.greetExtraTerrestrials('Martians'))
+//Finish Guess the Number Game
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if (this.lives == 0){
+      return Error.isError()
+    }
+    if (n === this.number){
+      return true
+    }
+    else{
+      this.lives --
+      return false
+    }
+    
+  }
+}
