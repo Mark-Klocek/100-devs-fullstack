b0ea01db4bb0e5291bd2630b4d94d05ac948e27c
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-17 09:02:49 -0700

    code wars: basic subclasses - Adam and Eve

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -460,3 +460,21 @@ class Guesser {
     
   }
 }
+//Basic Subclasses - Adam and Eve
+class God{
+/**
+ * @returns Human[]
+ */
+  static create(){
+    return [new Man(), new Woman()]
+  }
+}
+class Human{
+  
+}
+class Man extends Human{
+  
+}
+class Woman extends Human{
+  
+}
