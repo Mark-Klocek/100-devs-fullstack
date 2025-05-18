aef7f06012175898eb5e6ec63fb59e75ff23fc3e
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 11:10:26 -0700

    code wars: The spiraling box

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -524,3 +524,17 @@ function explode(x){
   }
   return returnArray
 }
+console.log(explode(['a','b32']))
+//The 'spiraling' box
+function createBox(m, n) {
+  returnArray = []
+  for (let i =0; i < n; i++){
+    pushArray = []
+    for(let j = 0; j<m;j++){
+      pushArray.push(Math.min(i,j,n-1-i,m-1-j)+1)
+    }
+    returnArray.push(pushArray)
+  }
+  return returnArray
+}
+console.log(createBox(5,5))
