012bddeed1974c3d63b37a2166e67092dd1541bb
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 14:39:20 -0700

    100 devs class 26 class work 1

--- 100 devs/Class 26/class26-materials/objects-lost-galaxy/js/main.js
+++ 100 devs/Class 26/class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,18 @@
 //Create a dog object that has four properties and three methods
+pizza = {}
+pizza.shape = 'Round'
+pizza.size = 'large'
+pizza.toppings = ['pepperoni']
+pizza.price = 12.50
+
+pizza.sayPrice = function (){
+    return `Your pizza cost $${this.price}`
+
+}
+pizza.description = function(){
+    return `You ordered a ${this.size} pizza with ${this.toppings} toppings.`
+}
+
+console.log(pizza.description())
+pizza.toppings.push('anchovies')
+console.log(pizza.description())
