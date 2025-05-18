cefc40ca009f04378f49f27e339279a8d70ce897
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 12:14:09 -0700

    code wars: Name Array Capping

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -548,3 +548,17 @@ function evenChars(string) {
   return returnArray
 }
 console.log(evenChars('asdasdasd'))
+// Name Array Capping
+function capMe1(names) {
+  return names.map((element,_) => {
+    splitElement = element.split('')
+    return splitElement.map((element,index)=> index == 0? element = element.toUpperCase():element = element.toLowerCase()).join('')
+     
+    
+  })
+}
+function capMe(names){
+  return names.map((element) => element.charAt(0).toUpperCase() + element.substring(1).toLowerCase())
+}
+console.log(capMe(['JOE', 'neNeLsOnlson', 'JuRiE']))
+
