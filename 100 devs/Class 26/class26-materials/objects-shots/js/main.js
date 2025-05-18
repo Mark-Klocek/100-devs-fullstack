8b32900c24c9b597de0dcc331158ed64e50491da
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 16:28:31 -0700

    100 devs class 26 class work 4

--- 100 devs/Class 26/class26-materials/objects-shots/js/main.js
+++ 100 devs/Class 26/class26-materials/objects-shots/js/main.js
@@ -1,1 +1,20 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click',drinkName)
+
+function drinkName(){
+    let newDrinkName = document.querySelector('input').value
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${newDrinkName}`)
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+    console.log(data.drinks[0])
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+}
+
+
+
+
