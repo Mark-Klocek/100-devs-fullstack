2c580dbf9fdd5b049f7415386c3189bd790c2470
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 15:09:54 -0700

    100 devs class 26 class work 2

--- 100 devs/Class 26/class26-materials/objects-constructor-fun/js/main.js
+++ 100 devs/Class 26/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,19 @@
 //Create a constructor with 4 properties and 3 methods
+function Pizza(shape,size,toppings,price) {
+    this.shape = shape
+    this.size = size
+    this.toppings = toppings
+    this.price = price
+
+    sayPrice = function (){
+        return `Your pizza cost $${this.price}`
+
+    }
+    description = function(){
+        return `You ordered a ${this.size} pizza with ${this.toppings} toppings.`
+    }
+}
+
+let myPizza = new Pizza('square','small',['cheeze','meat','veggies'],10)
+
+console.log(myPizza.toppings)
