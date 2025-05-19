eaece56c5d7d59a5b777e30e9ca6dbd7a7a5913a
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-18 18:22:50 -0700

    code wars: filter unused digits

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -571,3 +571,14 @@ function wordSearch(query, seq){
 }
   
 console.log(wordSearch("abcd", ["za", "aB", "Abc", "zAB", "zbc"]))
+//Filter unused digits
+function unusedDigits(...arr) {
+  let returnArray = arr.join('').split('').sort((a,b)=> a-b).join('')
+  returnString = ''
+  for (i = 0; i<=9; i++){
+    returnArray.includes(i)? '':returnString += i
+  }
+  
+  return returnString
+}
+console.log(unusedDigits(12, 34, 56, 78))
