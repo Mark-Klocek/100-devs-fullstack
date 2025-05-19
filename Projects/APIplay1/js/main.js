d21067dd6738587a19179a165472762af4ad8e0f
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-19 13:01:20 -0700

    playing with nasa API more

--- Projects/APIplay1/js/main.js
+++ Projects/APIplay1/js/main.js
@@ -6,6 +6,7 @@ fetch(`https://api.nasa.gov/planetary/apod?date=2024-12-14&api_key=${API_KEY}`)
   .then(data => {
     document.querySelector('img').src=data.url
     document.querySelector('h2').innerText = data.explanation
+    document.querySelector('h1').innerText = data.title
     console.log(data)    
 })
   .catch(err => console.error('Error:', err));
