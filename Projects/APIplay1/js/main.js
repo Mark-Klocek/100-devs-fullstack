d62cff7c6870fe5d7959ee47e09b385211730231
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-19 12:53:07 -0700

    100 devs nasa api homework

--- Projects/APIplay1/js/main.js
+++ Projects/APIplay1/js/main.js
@@ -1,1 +1,9 @@
+import { API_KEY } from "./config.js";
+console.log('API_KEY:', API_KEY);
+
+fetch(`https://api.nasa.gov/planetary/apod?date=2024-12-12&api_key=${API_KEY}`)
+
+  .then(res => res.json())
+  .then(data => document.querySelector('img').src=data.hdurl)
+  .catch(err => console.error('Error:', err));
 
