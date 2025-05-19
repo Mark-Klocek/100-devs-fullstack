49b6747a7d98b86359347e5fc8a6c2e84db8436a
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-19 15:24:46 -0700

    100 devs class 27 class work 2

--- 100 devs/Class 27/class27-materials/local-storage-botScore-button/js/main.js
+++ 100 devs/Class 27/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,14 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+document.querySelector('button').addEventListener('click', addAnothaOne)
+if (!localStorage.getItem('botScore')){
+    localStorage.setItem('botScore', 0)
 
+}
+
+function addAnothaOne(){
+    let botScoreVal = localStorage.getItem('botScore')
+    botScoreVal ++
+    localStorage.setItem('botScore',botScoreVal)
+    document.querySelector('h2').innerText = localStorage.getItem('botScore')
+    console.log(localStorage.getItem('botScore'))
+}
 
