7bc4aba35a0da2d0f72214b9b10e9aa49053f4f3
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-19 09:32:57 -0700

    code wars: 7kyu building blocks

--- 100 devs/Codewars/kyu_8_codwars.js
+++ 100 devs/Codewars/kyu_8_codwars.js
@@ -668,3 +668,31 @@ class Dog extends Animal {
     return `Hello ${this.master}`
   }
 }
+//Building Blocks
+class Block{
+
+  constructor(data){
+    this.width = data[0]
+    this.length = data[1]
+    this.height = data[2]
+  }
+  
+  getWidth(){
+    return this.width
+  }
+  
+  getLength(){
+    return this.length
+  }
+  
+  getHeight(){
+    return this.height
+  }
+  
+  getVolume(){
+    return (this.length * this.width * this.height)
+  }
+  getSurfaceArea(){
+    return (2*((this.length*this.width)+(this.length*this.height)+(this.width*this.height)))
+  }
+}
