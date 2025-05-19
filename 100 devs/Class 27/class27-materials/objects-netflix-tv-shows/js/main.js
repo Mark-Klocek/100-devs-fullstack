09d43476187673d2e8806a8341b960aeb9b7f4a2
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-19 13:54:25 -0700

    100 devs class 27 class work 1

--- 100 devs/Class 27/class27-materials/objects-netflix-tv-shows/js/main.js
+++ 100 devs/Class 27/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,31 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixTvShow{
+    constructor(genre,type,rating,length){
+        this.genre=genre
+        this.type=type
+        this.rating=rating
+        this.length=length
+    }
+    isHighRated(){
+        if (this.rating > 9){
+            return 'This is an amazing movie!'
+        }
+        else{
+            return 'This movie could be better'
+        }
+    }
+    isItAMovie(){
+        if(type == 'movie'){
+            return 'This is a movie'
+        }
+        else{
+            return 'this is a tv show'
+        }
+    }
+    
+}
 
+let theNun = new NetflixTvShow('horror','movie',9.5,'2 hours')
+
+console.log(theNun.isHighRated())
