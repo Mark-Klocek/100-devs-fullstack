f0656aea439f8138aef9ba0a3bd196cac499f1d8
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-20 12:36:31 -0700

    Class 27 home Art Institute of Chicago API - Called initial API to get art ID then used that ART_ID to pull up specific info about that art

--- Projects/class 27 api projects/project1/js/main.js
+++ Projects/class 27 api projects/project1/js/main.js
@@ -1,1 +1,20 @@
+document.querySelector('button').addEventListener('click',getArt)
 
+async function getArt(){
+    let ART_ID = null
+    await fetch('https://api.artic.edu/api/v1/artworks')
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data.data[0])
+            ART_ID = data.data[0].id
+            //console.log(ART_ID)
+        })
+            
+        .catch(err=>(`error: ${err}`))
+
+    let url = `https://api.artic.edu/api/v1/artworks/${ART_ID}?fields=id,title,image_id`
+    await fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => (`error: ${err}`))
+}
