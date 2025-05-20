6e176fb24637a950a85cfbe61a9fc67a364b5745
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-20 13:12:22 -0700

    Clicking the button now fetches the img_id from the art_id data, then fetches the config and data to build the url to the img

--- Projects/class 27 api projects/project1/js/main.js
+++ Projects/class 27 api projects/project1/js/main.js
@@ -2,19 +2,30 @@ document.querySelector('button').addEventListener('click',getArt)
 
 async function getArt(){
     let ART_ID = null
+    let BASE_URL = null
+    let IMAGE_ID = null
+    let URL_ENDING = '/full/843,/0/default.jpg'
     await fetch('https://api.artic.edu/api/v1/artworks')
         .then(res => res.json())
         .then(data => {
             //console.log(data.data[0])
-            ART_ID = data.data[0].id
+            ART_ID = data.data[5].id
             //console.log(ART_ID)
         })
             
         .catch(err=>(`error: ${err}`))
 
-    let url = `https://api.artic.edu/api/v1/artworks/${ART_ID}?fields=id,title,image_id`
-    await fetch(url)
+    let ART_URL = `https://api.artic.edu/api/v1/artworks/${ART_ID}?fields=id,title,image_id`
+    await fetch(ART_URL)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            BASE_URL = data.config.iiif_url
+            IMAGE_ID = data.data.image_id
+
+        })
         .catch(err => (`error: ${err}`))
+    let IMG_URL = `${BASE_URL}/${IMAGE_ID}${URL_ENDING}`
+    document.querySelector('img').src = IMG_URL
+    console.log(`${IMG_URL}`)
 }
