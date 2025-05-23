57bca0d9c1a58ee32ff0173a4ece541ab6173ce5
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-23 08:54:46 -0700

    100 devs class 28: class work 1

---
+++ 100 devs/Class 28/class28-materials/dnd/js/main.js
@@ -0,0 +1,18 @@
+//Example fetch using DnD5eAPI - place subclasses in ul
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = `https://www.dnd5eapi.co/api/2014/spells/${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        data.subclasses.forEach(element => document.querySelector('ul').innerText += element.name +'\n')
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
