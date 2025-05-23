d806ca8bd4de5427184d1deed9ee15ffbe39a6f6
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-23 09:54:52 -0700

    code wars class 28 class work 2

--- 100 devs/Class 28/class28-materials/dnd/js/main.js
+++ 100 devs/Class 28/class28-materials/dnd/js/main.js
@@ -2,14 +2,15 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/2014/spells/${choice}`
-
+    const choice = document.querySelector('input').value.replaceAll(' ', '-').toLowerCase()
+    const url = `https://www.dnd5eapi.co/api/2014/spells/${choice}`
+    document.querySelector('ul').innerHTML = ''
   fetch(url)
+    
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        data.subclasses.forEach(element => document.querySelector('ul').innerText += element.name +'\n')
+        data.subclasses.forEach(element => document.querySelector('ul').innerHTML += `<li>${element.name}</li>`)
       })
       .catch(err => {
           console.log(`error ${err}`)
