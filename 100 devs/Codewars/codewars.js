87439e396c92eca9cca42b3c81de2761315b9062
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-24 10:58:02 -0700

    code wars: 7 kyu shift left

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -769,3 +769,15 @@ function digits(n) {
   return String(n).length
 }
 console.log(digits(9876543210))
+//7 kyu shift left
+function shiftLeft(s, t){
+  let i = 0
+  while (s !== t){
+    
+    s.length >= t.length ? s = s.slice(1) : t = t.slice(1)
+    i++ 
+  }
+  return i
+}
+console.log(shiftLeft('1','1'))
+
