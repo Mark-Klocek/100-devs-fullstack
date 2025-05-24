2564b13a7d230078f184bd9016ce72a423fd2901
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-24 08:34:56 -0700

    code wars: 7 kyu Sum of Odd Cubed Numbers

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -731,9 +731,16 @@ class Quark{
 }
 //7 kyu Thinkful - Re-open class
 String.prototype.myNewMethod = function(){ return this.toUpperCase()}
-//7 kyu Can this object fly? Balloons in "Up" and real life.
-function Journey(object, crew, balloons) {
-  this.object = object
-  this.crew = crew
-  this.balloons = balloons
+//7 kyu Sum of Odd Cubed Numbers
+function cubeOdd(arr) {
+  if (arr.some(element => typeof element != 'number')){
+    return undefined
+  }
+  return arr.reduce((acc,c)=> {
+    if (c % 2 != 0){
+      acc += Math.pow(c,3)
+    }
+    return acc
+  },0)
 }
+console.log(cubeOdd(["a",12,9,"z",42]))
