981c10fd74d4b93ef5f8eee26e7dfe2fc84832c2
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-25 10:23:37 -0700

    100 devs class 29 class work 2

--- 100 devs/Class 29/class29-materials/boulder-badge/js/main.js
+++ 100 devs/Class 29/class29-materials/boulder-badge/js/main.js
@@ -1,8 +1,8 @@
 // Variables
 //You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
-let candiesForBulbasaur = 11
-let candiesForCaterpie = 6
-let candiesForWeedle = 6
+let candiesForBulbasaur = 16-5
+let candiesForCaterpie = 7-1
+let candiesForWeedle = 7-1
 let totalCandiesNeeded = candiesForBulbasaur + candiesForCaterpie + candiesForWeedle
 //Conditionals And Functions
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
--- 100 devs/Class 29/class29-materials/cascade-badge/js/main.js
+++ 100 devs/Class 29/class29-materials/cascade-badge/js/main.js
@@ -1,11 +1,31 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
-
+function reverseList(array){
+    return array.reverse()
+}
+console.log(reverseList([22, -6, 32, 82, 9, 25]))
 //Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
-
+function aGreaterThanb(arr1,arr2){
+    arr1sum = arr1.reduce((acc,c) => acc+c**2,0)
+    arr2sum = arr2.reduce((acc,c) => acc += c**3,0)
+    return arr1sum>arr2sum
+}
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
+function specialNumber(arr){
+    return arr.filter((element,index) =>{
+         if (element % index == 0){
+            return element
+         }
+    })
+}
+console.log(specialNumber([68, -1, 1, -7, 10, 10]))
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
+function sumOfAll(arr){
+    return arr.reduce((acc,c) => acc += Number(c),0 )
+}
+console.log(sumOfAll([22, -6, '32', 82, 9, '25']))
