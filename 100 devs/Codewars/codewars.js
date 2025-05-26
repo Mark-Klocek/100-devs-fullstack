c308c70ccafc7b330a6fb494338ab4121c0bf0bd
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-26 09:37:17 -0700

    code wars: 7 kyu What comes after?

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -866,3 +866,10 @@ function solve2(a,b){
   return returnString
 };
 console.log(solve2("xyab","xzca"))
+//7 kyu What comes after? 
+function comes_after(str,l) {
+  returnString = ''
+  str.toLowerCase().split('').forEach((_,index,array)=> (array[index-1] == l && array[index]!=  ' ' && (array[index] >= 'a' && array[index] <= 'z')) ?returnString += str.split('')[index]: '')
+  return returnString
+}
+console.log(comes_after("Every Sunday, she reads newspapers.",'s'))
