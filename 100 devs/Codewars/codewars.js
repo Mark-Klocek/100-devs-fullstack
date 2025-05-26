92cb4a3c8ca7c4cb7927b6ca7778e093a02a74cc
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-26 08:53:50 -0700

    code wars: 7kyu uinque string characters

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -858,3 +858,11 @@ function scrabbleScore(str){
   return str.replaceAll(' ','').toUpperCase().split('').reduce((acc,c)=>acc+= $dict[c],0)
 }
 console.log(scrabbleScore("st re et"))
+// 7kyu unique string characters
+function solve2(a,b){
+  returnString = ''
+  a.split('').forEach(element=>!b.split('').includes(element) ? returnString += element: '')
+  b.split('').forEach(element=>!a.split('').includes(element) ? returnString += element: '')
+  return returnString
+};
+console.log(solve2("xyab","xzca"))
