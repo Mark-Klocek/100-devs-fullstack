b2d96f0da653d6a0a5ea82d7769dac4f343c1a7d
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-26 13:13:42 -0700

    100 devs class 30 class work 1

--- 100 devs/Class 30/class30-materials/objects-espresso-machine/js/main.js
+++ 100 devs/Class 30/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,30 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine{
+    constructor(size,caffeine,price,drink){
+        this.size = size
+        this.caffeine = caffeine
+        this.price = price
+        this.drink = drink
+    }
+
+    saySize(){
+        return `You ordered a ${this.size} drink`
+    }
+
+    sayCaffeine(){
+        return `Your drink has ${this.caffeine}mg of caffeine`
+    }
+
+    sayPrice(){
+        return `You owe $${this.price}`
+    }
+    
+    sayFullOrder(){
+        return `You've ordered a ${this.size} ${this.drink} which has ${this.caffeine}mg of caffeine. That'll cost you $${this.price}`
+    }
+
+}
+
+let myCoffee = new EspressoMachine('Large','300','6.50','Espresso')
+
+console.log(myCoffee.sayFullOrder())
