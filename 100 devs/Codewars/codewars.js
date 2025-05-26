4e914ccd794c5b5564e2d10a6783e6834ff5b232
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-26 10:05:43 -0700

    code wars: 7 kyu cats in hats

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -873,3 +873,17 @@ function comes_after(str,l) {
   return returnString
 }
 console.log(comes_after("Every Sunday, she reads newspapers.",'s'))
+//7 kyu cats in hats
+function height(n) {
+  totalCat = 2000000
+  previousCat = 2000000
+  for (i=0; i<n;i++){
+    currentCat = previousCat / 2.5
+    previousCat = currentCat
+    totalCat += currentCat
+    
+  }
+
+  return totalCat.toFixed(3)
+}
+console.log(height(7))
