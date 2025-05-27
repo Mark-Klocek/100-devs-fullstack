0d5ae4926c6ef2d6df153369bef2947f4daf046c
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 11:19:42 -0700

    100 devs class 30 eloquent javascript HW question2

--- 100 devs/Class 30/js/main.js
+++ 100 devs/Class 30/js/main.js
@@ -16,3 +16,38 @@ class Vec{
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 console.log(new Vec(1, 2).minus(new Vec(2, 3)))
 console.log(new Vec(3, 12).length);
+
+class Group{
+    constructor(arr=[]){
+        this.arr = arr
+    }
+    static from(arr){
+        let group = new Group()
+        for (let i=0;i<arr.length;i++){
+            group.add(arr[i])
+        }
+        return group
+    }
+    add(n){
+        if (!this.arr.includes(n)){
+            this.arr.push(n)
+        }
+    }
+    delete(n){
+        if (this.arr.includes(n)){
+            delete this.arr[this.arr.indexOf(n)]
+        }
+    }
+    has(n){
+        return this.arr.includes(n)
+    }
+}
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
