aec8eff202727dcbbf4674bb5753c47ccd188e75
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 09:38:49 -0700

    code wars: 7 kyu Is every value in the array an array?

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -944,3 +944,15 @@ function makeLatinSquare(n) {
   return returnArr;
 }
 console.log(makeLatinSquare(7))
+// 7kyu Is every value in the array an array?
+function arrCheck(arr){
+  for (element of arr){
+    if (!Array.isArray(element)){
+      return false
+    }
+  }
+  return true
+}
+
+
+console.log(arrCheck([[],[]]))
