17ff4fd7b971be141035242d16d31f4894d409e5
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 15:49:01 -0700

    code wars: 8 kyu speedcode#2 array madness

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -961,3 +961,8 @@ sortme = function( names ){
   return names.sort()
 }
 console.log(sortme(['one', 'two', 'three' ]))
+//8 kyu Speedcode#2 - Array Madness
+function arrayMadness(a, b) {
+  return a.reduce((acc,c) => acc+= c**2,0) > b.reduce((acc,c)=>acc+= c**3,0)
+}
+console.log(arrayMadness([4,5,6],[1,2,3]))
