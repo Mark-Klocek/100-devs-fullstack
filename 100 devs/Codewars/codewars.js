49fc7f1672293c4d5c524ef9fd86c80804e7aa5d
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 16:08:54 -0700

    7 kyu mexican wave

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -983,3 +983,19 @@ function ìsZeroBalanced(n){
 }
 
 console.log(ìsZeroBalanced(([3, -3, 5, -5, 7, -7])))
+//6 kyu Mexican Wave
+function wave(str){
+  
+  returnArr = []
+  for (i=0;i<str.length;i++){
+    
+    let arr = [...str]
+    if (arr[i].charCodeAt(0) -96 > 0 && arr[i].charCodeAt(0) - 96 < 27){
+      arr[i] = arr[i].toUpperCase()
+      returnArr.push(arr.join(''))
+    }
+    
+  }
+  return returnArr
+}
+console.log(wave("two words"))
