48dcd376de2b970e22deb48d5d28a03c3c8d69aa
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 13:54:01 -0700

    100 devs class 31 class work one - class inheritence exercise

--- 100 devs/Class 31/class31-materials/objects-child-class/js/main.js
+++ 100 devs/Class 31/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,36 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+class Animal{
+    constructor(name){
+        this._name = name
+    }
+    get name(){
+        return this._name
+    }
+    speak(){
+        console.log(`${this._name} makes a sound`)
+    }
+}
 
+class Horse extends Animal{
+    constructor(name,speed,age,sex){
+        super(name)
+        this.speed = speed
+        this.age = age
+        this.sex = sex
+    }
+
+    speedCheck(){
+        return this.speed > 20 ? `This is a fast horse!` : `This horse needs some work.`
+    }
+
+    describe(){
+        return `${this.name} is a ${this.age} year old ${this.sex} horse. ${this.speedCheck()} `
+    }
+}
+
+let molly = new Horse('Molly',25,4,'female')
+
+console.log(molly.describe())
+
+let ralph = new Horse('Ralph',5,2,'male')
+
+console.log(ralph.describe())
