496219bd7c8522ae2c8d1452c62abd028340a0a3
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 11:52:24 -0700

    100 devs class 30 thejsway hw question 1

--- 100 devs/Class 30/js/main.js
+++ 100 devs/Class 30/js/main.js
@@ -51,6 +51,20 @@ group.add(10);
 group.delete(10);
 console.log(group.has(10));
 // → false
-for (let value of Group.from(["a", "b", "c"])) {
-  console.log(value);
+class Dog{
+    constructor(name,type,size){
+        this.name = name
+        this.species = type
+        this.size = size
+    }
+    bark(){
+        return this.size > 60 ? `Grrr! Grrr!`:`Woof! Woof!`
+    }
 }
+
+const fang = new Dog("Fang", "boarhound", 75);
+console.log(`${fang.name} is a ${fang.species} dog measuring ${fang.size}`);
+console.log(`Look, a cat! ${fang.name} barks: ${fang.bark()}`);
+const snowy = new Dog("Snowy", "terrier", 22);
+console.log(`${snowy.name} is a ${snowy.species} dog measuring ${snowy.size}`);
+console.log(`Look, a cat! ${snowy.name} barks: ${snowy.bark()}`);
