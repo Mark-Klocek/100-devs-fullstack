67f2b21adc34b0f070afc2aa6d32b52869a08230
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 16:16:21 -0700

    code wars://7 kyu Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?

--- 100 devs/Codewars/codewars.js
+++ 100 devs/Codewars/codewars.js
@@ -999,3 +999,19 @@ function wave(str){
   return returnArr
 }
 console.log(wave("two words"))
+//7 kyu Coding Meetup #3 - Higher-Order Functions Series - Is Ruby coming?
+function isRubyComing(list) {
+  rubyComing = false
+  list.forEach(element => element.language == 'Ruby' ? rubyComing = true:'' )
+  return rubyComing
+}
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+]
+var list2 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' } 
+];
+console.log(isRubyComing(list2))
