e407383c9a1a01a2dfda6f08b1568c5a2a43c630
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-27 11:02:39 -0700

    100 devs class 30 eloquent javascript HW question1

--- 100 devs/Class 30/js/main.js
+++ 100 devs/Class 30/js/main.js
@@ -1,1 +1,18 @@
-
+class Vec{
+    constructor(x,y){
+        this.x=x
+        this.y=y
+    }
+    plus(obj){
+        return new Vec(obj.x+this.x,obj.y+this.y)
+    }
+    minus(obj){
+        return new Vec(this.x-obj.x,this.y-obj.y)
+    }
+    get length(){
+        return Math.sqrt(this.x**2+this.y**2)
+    }
+}
+console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)))
+console.log(new Vec(3, 12).length);
