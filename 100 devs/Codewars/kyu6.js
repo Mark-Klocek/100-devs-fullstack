d1e3f8871957371d4b517d14b03f0362faaad83b
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-28 10:17:50 -0700

    code wars: 6 kyu Simple Sentences

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -1,7 +1,7 @@
 //
 //5-28-25
 //
-//5kyu Moving Zeroes To The End
+//5 kyu Moving Zeroes To The End
 function moveZeros(arr) {
     let nonZeroArray = []
     let zeroArray = []
@@ -11,3 +11,24 @@ function moveZeros(arr) {
 }
 
 console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
+//6 kyu Simple Sentences
+function makeSentence(parts) {
+    parts.forEach((element,index) => {
+        
+        if (element.length === 1 && (element < 'a' || element > 'Z') && isNaN(Number(element)) && element != '.'){
+            console.log(`${element}: ${index}: ${typeof element}`)
+            parts[index -1] = parts[index -1] += element
+            parts.splice(index,1)
+        }
+    })
+    
+    let sentence = parts.join(' ')
+    while (sentence.endsWith('.') || sentence.endsWith(' ')){
+        sentence = sentence.slice(0,-1)
+    }
+    
+    return sentence+'.'
+    
+}
+
+console.log(makeSentence(['4','4',4, ',', 'my', 'dear','.','.','.']))
