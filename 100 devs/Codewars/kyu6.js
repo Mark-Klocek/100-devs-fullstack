2dc3180d8704f1aa1da725be71194c85d92c3a75
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-28 09:00:33 -0700

    code wars: kyu 5 Moving Zeroes To The End

---
+++ 100 devs/Codewars/kyu6.js
@@ -0,0 +1,13 @@
+//
+//5-28-25
+//
+//5kyu Moving Zeroes To The End
+function moveZeros(arr) {
+    let nonZeroArray = []
+    let zeroArray = []
+    arr.forEach((element,index)=>element === 0 ? zeroArray.push(element):nonZeroArray.push(element))
+    zeroArray.forEach(element => nonZeroArray.push(element))
+    return nonZeroArray
+}
+
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
