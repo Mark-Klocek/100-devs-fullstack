75cbc3db510cbbbe1e0e639d37cc2e6dd5c15fb1
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-29 09:02:49 -0700

    code wars: // 6 kyu Most Consecutive Zeros of a Binary Number

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -79,15 +79,58 @@ console.log(inSphere([66.14272,66.57249,0.42063,48.18087,15.88034,15.90368], 90)
 // 6 kyu Convert integer to Whitespace format
 function whitespaceNumber(n) {
     returnString = ''
-    if (n > 0) returnString += ' 1'
+    if (n > 0) returnString += ' '
     
     nBinary = n.toString(2)
     if (n < 0) {
         nBinary = nBinary.slice(1)
-        returnString += '\\t'
+        returnString += '\t'
     }
 
-    nBinary.split('').forEach((element)=> element == '1'? returnString+= '\\t':returnString+= ' ')
-    return returnString += '\\n';
+    nBinary.split('').forEach((element)=> element == '1'? returnString+= '\t':returnString+= ' ')
+    return returnString += '\n';
   }
 console.log((whitespaceNumber(0)))
+// 6 kyu Most Consecutive Zeros of a Binary Number
+function maxConsecZeros(n){
+    const numbersToWords = {
+  0: "Zero",
+  1: "One",
+  2: "Two",
+  3: "Three",
+  4: "Four",
+  5: "Five",
+  6: "Six",
+  7: "Seven",
+  8: "Eight",
+  9: "Nine",
+  10: "Ten",
+  11: "Eleven",
+  12: "Twelve",
+  13: "Thirteen",
+  14: "Fourteen",
+  15: "Fifteen",
+  16: "Sixteen",
+  17: "Seventeen",
+  18: "Eighteen",
+  19: "Nineteen",
+  20: "Twenty"
+    }
+  nBinary = Number(n).toString(2)
+  binaryList = nBinary.split('')
+  currentStreak = 0
+  highestStreak = 0
+  console.log(binaryList)
+  binaryList.forEach((element) => {
+  if (element === '0') {
+    currentStreak++;
+  } else {
+    highestStreak = Math.max(highestStreak, currentStreak);
+    currentStreak = 0;
+  }
+})
+  
+  highestStreak =  Math.max(currentStreak,highestStreak)
+  return numbersToWords[highestStreak]
+}
+console.log(maxConsecZeros("180"))
