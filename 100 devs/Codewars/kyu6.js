5a2fd935837992f505a39ee0dc8ff97ec05d4235
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-29 10:33:19 -0700

    6 kyu frequency analysis with buckets

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -157,3 +157,32 @@ const frame = (text, char) => {
   return returnString += `${topAndBottomBorder}`
 };
 console.log(frame(['Small','frame'], '+'))
+//6 kyu Frequency Analysis With Buckets
+
+function bucketize(arr) {
+  let obj = {};
+  arr.forEach((element) => {
+    if (obj[element]) {
+      obj[element] += 1;
+    } 
+    else {
+      obj[element] = 1;
+    }
+  });
+  returnArray = []
+  for (i=0;i<=arr.length;i++){
+    returnArray.push(null)
+  }
+  for (pairs in obj){
+    if(returnArray[obj[pairs]] == null){
+        returnArray[obj[pairs]] = [Number(pairs)]
+    }
+    else{
+        returnArray[obj[pairs]].push(Number(pairs))
+    }
+  }
+  return returnArray;
+}
+
+inputArrTwo = [4,4,8,8]
+console.log(bucketize(inputArrTwo))
