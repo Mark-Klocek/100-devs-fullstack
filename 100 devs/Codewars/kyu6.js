947487c02d8029b1677ae134a35cffe5b1dab066
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-30 09:11:33 -0700

    7kyu count the ones

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -199,3 +199,10 @@ return [
 };
 
 console.log(crossover('0000011111','1111100000',5))
+//7 kyu count the ones
+function hammingWeight(x){
+  count = 0
+  x.toString(2).split('').forEach(element=> element === '1' ? count ++:'')
+  return count
+}
+console.log(hammingWeight(10))
