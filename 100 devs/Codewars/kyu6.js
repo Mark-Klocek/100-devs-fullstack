fb427f977aea925ac41f5e6be174362158795751
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-30 08:58:29 -0700

    code wars: 7 kyu genetic algorithm series - #3 crossover

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -186,3 +186,16 @@ function bucketize(arr) {
 
 inputArrTwo = [4,4,8,8]
 console.log(bucketize(inputArrTwo))
+//
+//5-29-25
+//
+// 7kyu Genetic Algorithm Series - #3 crossover
+const crossover = (chromosome1, chromosome2, index) => {
+  
+return [
+    chromosome1.slice(0,index)+chromosome2.slice(index),
+    chromosome2.slice(0,index)+chromosome1.slice(index)
+]
+};
+
+console.log(crossover('0000011111','1111100000',5))
