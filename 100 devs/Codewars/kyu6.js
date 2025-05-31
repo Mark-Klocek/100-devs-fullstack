fc8d9e7d9030017212296186e51222147634ce76
Mark Klocek <Mark.Klocek@GMail.com>
2025-05-31 09:47:30 -0700

    code wars: 8 kyu neutralise

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -206,3 +206,27 @@ function hammingWeight(x){
   return count
 }
 console.log(hammingWeight(10))
+//
+//5-28-25
+//
+//8 kyu Neutralisation
+function neutralise(s1, s2) {
+  returnStr = ''
+  s1 = s1.split('')
+  s2 = s2.split('')
+  s1.forEach((element,index) => {
+    if (element === '-' && s2[index] === '-'){
+      returnStr += '-'
+    }
+    else if (element === '+' && s2[index] === '+'){
+      returnStr += '+'
+    }
+    else{
+      returnStr += '0'
+    }
+  })
+  return returnStr;
+  
+}
+
+console.log(neutralise("--++--", "++--++"))
