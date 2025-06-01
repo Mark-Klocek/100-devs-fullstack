adc2cb8633b2af3fed780c61e5e12fec940ec79c
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-01 10:43:48 -0700

    code wars 8 generate user links

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -379,3 +379,8 @@ function isOpposite(s1, s2) {
   if (s1.length !== s2.length) return false;
   return s1.split('').every((char, i) => char !== s2[i] && char.toLowerCase() === s2[i].toLowerCase());
 }
+//8 kyu generate user links
+function generateLink(user) {
+  return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
+}
+
