ccc0db895dd2036845ca3a81a0851518d558d73f
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-01 10:41:19 -0700

    code wars 8 kyu welcome to the city

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -360,3 +360,16 @@ function cannonsReady(gunners) {
     ? 'Fire!'
     : 'Shiver me timbers!';
 }
+// 8kyu [BUG] XCOM-409: Flight distance of Interceptor planes is miscalculated
+function travelDistance(avgSpeed, travelTime) {
+  const KM_PER_NAUTICAL_MILE = 1.852;
+  const travelHours = travelTime / 60;
+  const travelNauticalMiles = avgSpeed * travelHours;
+  const travelKms = travelNauticalMiles * KM_PER_NAUTICAL_MILE;
+
+  return travelKms;
+}
+//8 kyu welcome to the city
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
