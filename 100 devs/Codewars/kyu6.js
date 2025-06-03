792887b3fe580c959d3df14003ef8c47ac454089
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-03 16:40:40 -0700

    code wars

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -430,3 +430,10 @@ function drawStairs(n) {
   }
   return stairs;
 }
+//drink about
+function peopleWithAgeDrink(age) {
+  if (age < 14) return "drink toddy";
+  if (age < 18) return "drink coke";
+  if (age < 21) return "drink beer";
+  return "drink whisky";
+}
