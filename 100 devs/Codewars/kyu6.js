6e50dac7d0cc8c70a80b0f1891a736c70c79947b
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-05 08:41:49 -0700

    code wars: 7 kyuPowers of 3

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -593,3 +593,12 @@ function abbreviate(string) {
   return wordArray.join('')
 }
 console.log(abbreviate("Go"))
+//7 kyu Powers of 3
+function largestPower(n){
+  let k = 0
+  while (3**k < n){
+    k += 1
+  }
+  return k-1
+}
+console.log(largestPower(3))
