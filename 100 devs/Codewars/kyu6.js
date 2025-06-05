deac84cfeaa708a043e17552880cff74a8bec01e
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-05 08:35:23 -0700

    code wars: //6 kyu Word a10n (abbreviation)

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -564,3 +564,32 @@ list1 = [
   { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
 ];
 console.log(getFirstPython(list1))
+//6 kyu Word a10n (abbreviation)
+function abbreviate(string) {
+  let stringArray = string.split('')
+  let wordArray = []
+  let word = ''
+  
+  stringArray.forEach(element =>{
+    if ((element >= 'a' && element <= 'z') || (element >= 'A' && element <= 'Z')){
+      word += element
+    }
+    else{
+      wordArray.push(word)
+      wordArray.push(element)
+      word = ''
+    }
+  })
+  if (wordArray[wordArray.length - 1] !== word){
+    wordArray.push(word)
+  }
+  
+  word = ''
+  wordArray.forEach((element,index)=>{
+    if (element.length >= 4){
+    wordArray[index] = `${element[0]}${element.length - 2}${element[element.length -1]}`
+    }
+  })
+  return wordArray.join('')
+}
+console.log(abbreviate("Go"))
