b9409cc9674c06ebcb128a82c2d5686d75d6bdb7
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-05 08:02:47 -0700

    /8 kyu Exclamation marks series #11

--- 100 devs/Codewars/kyu6.js
+++ 100 devs/Codewars/kyu6.js
@@ -533,3 +533,18 @@ class Kata {
     return length * width * height
   }
 }
+//
+//6-4-25
+//
+//8 kyu Exclamation marks series #11
+function replace(s) {
+  let returnArray = s.split('')
+  returnArray.forEach((element,index) => {
+    elementLower = element.toLowerCase()
+    if (elementLower === 'a'||elementLower === 'e'||elementLower === 'i'||elementLower === 'o'||elementLower === 'u'){
+      returnArray[index] = '!'
+    }
+  })  
+    return returnArray.join('');
+}
+console.log(replace("AEIOU"))
