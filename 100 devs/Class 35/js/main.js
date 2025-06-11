0ac4d01cb0b44c9636ba488d8cd933604bd71a6e
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-11 14:06:31 -0700

    100 devs class 35 home work error handling with promises

--- 100 devs/Class 35/js/main.js
+++ 100 devs/Class 35/js/main.js
@@ -5,7 +5,7 @@ let promise = new Promise(function(resolve, reject) {
   setTimeout(() => resolve(2), 1000);
 });
 
-promise.then(alert);
+promise.then(console.log('done'));
 //1- you can only have one resolve or reject per promise, so the first resolve is what will be displayed
 
 
@@ -18,4 +18,28 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve,ms))
 }
 
-delay(3000).then(() => alert('runs after 3 seconds'));
+delay(3000).then(() => console.log('runs after 3 seconds'));
+
+//Are these code fragments equal? In other words, do they behave the same way in any circumstances, for any handler functions?
+
+// promise.then(f1).catch(f2);
+
+// Versus:
+
+// promise.then(f1, f2);
+
+//No, because the top fragment has a catch, which only handles thrown errors, whereas the bottom promise handles two normal response functions that can handle either resolves or rejects
+
+//**CORRECTION the error is actually unhandled in the 2nd code blurb because it doesn't have a catch! */
+
+// Error in setTimeout
+
+// What do you think? Will the .catch trigger? Explain your answer.
+
+// new Promise(function(resolve, reject) {
+//   setTimeout(() => {
+//     throw new Error("Whoops!");
+//   }, 1000);
+// }).catch(alert);
+
+//No, it will not trigger because the catch is outside of the promise chain. It is outside of the executor.
