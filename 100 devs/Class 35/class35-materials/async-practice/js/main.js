6a5dcb6bf85372329a110fbd20f3ee5c0dd815a1
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-13 13:22:08 -0700

    100 devs class 35 async try/catch homework

--- 100 devs/Class 35/class35-materials/async-practice/js/main.js
+++ 100 devs/Class 35/class35-materials/async-practice/js/main.js
@@ -65,34 +65,34 @@
 //     .catch(err => console.log(err))
 
 //Code 06
-function houseOne(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 1')
-        }, 1000)
-    })
-}
-function houseTwo(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 2')
-        }, 5000)
-    })
-}
-function houseThree(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Paper delivered to house 3')
-        }, 2000)
-    })
-}
-houseOne()
-    .then(data => console.log(data))
-    .then(houseTwo)
-    .then(data => console.log(data))
-    .then(houseThree)
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+// function houseOne(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 1')
+//         }, 1000)
+//     })
+// }
+// function houseTwo(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 2')
+//         }, 5000)
+//     })
+// }
+// function houseThree(){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('Paper delivered to house 3')
+//         }, 2000)
+//     })
+// }
+// houseOne()
+//     .then(data => console.log(data))
+//     .then(houseTwo)
+//     .then(data => console.log(data))
+//     .then(houseThree)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
 
 //Code 07
 // function houseOne(){
@@ -126,13 +126,19 @@ houseOne()
 //     console.log(houseThreeWait)
 // }
 
-// getPaid()
+//getPaid()
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+
+    try{
+        const res = await fetch('https://dog.ceo/api/breeds/image/random')
+        const data = await res.json()
+        console.log(data)
+        
+    }catch(error){
+        console.log('test')
+    }
+}
+getACuteDogPhoto()
 
