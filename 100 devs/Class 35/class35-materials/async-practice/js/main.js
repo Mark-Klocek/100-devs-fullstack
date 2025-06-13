6479ee5b5fd6c858fcbc3fe67b7b357d9552bad9
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-13 12:01:32 -0700

    100 devs class 35 async practice 2

--- 100 devs/Class 35/class35-materials/async-practice/js/main.js
+++ 100 devs/Class 35/class35-materials/async-practice/js/main.js
@@ -3,7 +3,7 @@ function houseOne(){
     console.log('Paper delivered to house 1')
 }
 function houseTwo(){
-    setTimeout(()=>console.log('Paper delivered to house 2'),2000)
+    setTimeout(()=>console.log('Paper delivered to house 2'),0)
 }
 function houseThree(){
     console.log('Paper delivered to house 3')
