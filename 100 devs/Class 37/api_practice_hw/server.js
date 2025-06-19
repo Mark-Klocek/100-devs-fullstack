e6aa3c2ab2f27cee7fa7953d3749fed4808a5970
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-19 10:59:50 -0700

    practicing node

---
+++ 100 devs/Class 37/api_practice_hw/server.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const querystring = require('querystring')
+
+const server = http.createServer((request,response)=>{
+    const page = url.parse(request.url).pathname;
+    const params = querystring.parse(url.parse(request.url).query)
+
+    switch (page){
+        case '/':
+            fs.readFile('index.html', function(error,data){
+                response.writeHead(200, {'Content-type':'text/html'});
+                response.write(data);
+                response.end()
+
+            })
+        case '/otherpage':
+            fs.readFile('index.html', function(error,data){
+                response.writeHead(200, {'Content-type':'text/html'});
+                response.write(data);
+                response.end()
+
+            })
+    }
+
+})
