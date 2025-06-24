b7075a7d8072ed99e4debadad5305c90d302f8f4
Mark Klocek <Mark.Klocek@GMail.com>
2025-06-24 14:01:08 -0700

    rap names api able to make api calls based on query param

--- 100 devs/Class 38/rap-names-api/server.js
+++ 100 devs/Class 38/rap-names-api/server.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
-const PORT = 8000
+const PORT = 12000
 
-const savage = {
-    'age': 29, 
-    'birthName':'Sheyaa Bin Abraham-Joseph',
-    'birthLocation':'London, England',
+const rappers = {
+    '21 savage':{
+        'age': 29, 
+        'birthName':'Sheyaa Bin Abraham-Joseph',
+        'birthLocation':'London, England',
+    },
+    'chance the rapper':{
+        'age': 28, 
+        'birthName':'Chancelor Bennett',
+        'birthLocation':'Chicago, Illinois',
+    },
+    'dylan':{
+        'age': 28, 
+        'birthName':'Dylan',
+        'birthLocation':'Dylan',
+    }
 }
+    
 
 
 
@@ -14,8 +27,16 @@ app.get('/',(request,response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api',(request,response)=>{
-    response.json(savage)
+app.get('/api/:rapperName',(request,response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    console.log(rappers[rappersName])
+    if (rappers[rappersName]){
+        response.json(rappers[rappersName])
+    }
+    else{
+        response.json(rappers['dylan'])
+    }
+    
 })
 
 app.listen(PORT, ()=>{
