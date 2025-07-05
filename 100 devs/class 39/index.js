c5ed1d46f9f8cd7a173047f0158dc1e0e3ebb0f2
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-05 12:49:22 -0700

    class 39 homework 6 added api routes

--- 100 devs/class 39/index.js
+++ 100 devs/class 39/index.js
@@ -26,6 +26,31 @@ app.get('/',(request,response)=>{
 app.get('/api/notes',(request,response)=>{
     response.json(notes)
 })
+app.get('/api/notes/:id',(request,response)=>{
+    const id = request.params.id
+    const note = notes.find(note => note.id ===id)
+    if (note) {
+        response.json(note)
+    }else{
+        response.status(404).end()
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`Server running on PORT ${PORT}`)
 })
