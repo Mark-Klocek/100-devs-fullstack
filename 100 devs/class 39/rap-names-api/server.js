fdf6e239fdfe34bf38162d089d3a7913cabedb4c
Mark Klocek <Mark.Klocek@GMail.com>
2025-07-08 14:27:29 -0700

    class 39 work 4

--- 100 devs/class 39/rap-names-api/server.js
+++ 100 devs/class 39/rap-names-api/server.js
@@ -3,12 +3,19 @@ const app = express()
 
 const PORT = 8000
 
+const savage = {
+    'birth name': 29,
+    'birth location': 'sheya bin abraham',
+    'age' : 'London, England'
+}
 
 app.get('/',(request,response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-
+app.get('/api/',(request,response)=>{
+    response.json(savage)
+})
 
 
 
