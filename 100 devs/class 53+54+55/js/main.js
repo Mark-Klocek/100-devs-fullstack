24764d29c8f28eadfac2ab3b4cdecf4c2d65ed2e
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-20 12:20:26 -0700

    class 55 homework question 6 solution

--- 100 devs/class 53+54+55/js/main.js
+++ 100 devs/class 53+54+55/js/main.js
@@ -93,3 +93,21 @@ let sorted = copySorted(earr);
 
 console.log( sorted ); // CSS, HTML, JavaScript
 console.log( earr ); // HTML, JavaScript, CSS (no changes)
+
+// importance: 5
+
+// You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+// For instance:
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names = users.map(element =>{
+    return element.name
+})
+
+console.log( names ); // John, Pete, Mary
