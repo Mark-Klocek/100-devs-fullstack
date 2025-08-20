88886a92e4b1f3114f14334f325adc6bc483d8b6
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-20 12:47:13 -0700

    class 55 homework question 12 solution

--- 100 devs/class 53+54+55/js/main.js
+++ 100 devs/class 53+54+55/js/main.js
@@ -221,3 +221,35 @@ let strings = ["Hare", "Krishna", "Hare", "Krishna",
 ];
 
 console.log( unique(strings) ); // Hare, Krishna, :-O
+
+// Create keyed object from array
+// importance: 4
+
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+// Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+// For example:
+
+users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+function groupById(arr){
+    return arr.reduce((obj,value) =>{
+         obj[value.id]= value
+         return obj
+    },{})
+}
+let usersById = groupById(users);
+console.log(usersById)
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
