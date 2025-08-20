48aa71159ac02bbd11c32b72f6bb76382b57d9c9
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-20 11:57:11 -0700

    homework question 2 soltuion

--- 100 devs/class 53+54+55/js/main.js
+++ 100 devs/class 53+54+55/js/main.js
@@ -29,3 +29,23 @@ function camelize(str){
 console.log(camelize("background-color") == 'backgroundColor')
 console.log(camelize("list-style-image") == 'listStyleImage')
 console.log(camelize("-webkit-transition") == 'WebkitTransition')
+
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+// The function should not modify the array. It should return the new array.
+
+// For instance:
+
+let arr = [5, 3, 8, 1];
+function filterRange(arr,a,b){
+    
+    return arr.filter(element =>{
+        return (a <= element && element <= b)
+    })
+}
+
+let filtered = filterRange(arr, 1, 4);
+
+// Compare arrays by their string representation
+console.log(filtered.toString() === [3,1].toString()); // true if matching values
+
