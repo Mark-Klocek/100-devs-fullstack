9793d19480d45c19a29fc6dffb6a1096cce337fd
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-20 12:42:01 -0700

    class 55 homework question 11 solution

--- 100 devs/class 53+54+55/js/main.js
+++ 100 devs/class 53+54+55/js/main.js
@@ -199,3 +199,25 @@ function getAverageAge(arr){
     return total / arr.length
 }
 console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+
+// Filter unique array members
+// importance: 4
+
+// Let arr be an array.
+
+// Create a function unique(arr) that should return an array with unique items of arr.
+
+// For instance:
+
+function unique(arr) {
+  returnArray = []
+  arr.forEach(element => returnArray.includes(element)? '' : returnArray.push(element))
+  return returnArray
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(strings) ); // Hare, Krishna, :-O
