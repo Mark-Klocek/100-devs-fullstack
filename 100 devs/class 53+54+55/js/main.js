992e061383fc71bbd22f4a5ff5797c55274d160e
Mark Klocek <Mark.Klocek@GMail.com>
2025-08-20 12:38:36 -0700

    class 55 homework question 10 solution

--- 100 devs/class 53+54+55/js/main.js
+++ 100 devs/class 53+54+55/js/main.js
@@ -179,3 +179,23 @@ function shuffle(arr){
 shuffle(arr);
 
 console.log(arr)
+
+// Get average age
+// importance: 4
+
+// Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+
+// The formula for the average is (age1 + age2 + ... + ageN) / N.
+
+// For instance:
+
+john = { name: "John", age: 25 };
+pete = { name: "Pete", age: 30 };
+mary = { name: "Mary", age: 29 };
+
+arr = [ john, pete, mary ];
+function getAverageAge(arr){
+    let total = arr.reduce((acc,c)=>acc += c.age,0)
+    return total / arr.length
+}
+console.log( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
